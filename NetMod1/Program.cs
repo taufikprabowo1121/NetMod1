66b4150c54b42c2c7b04a245765d9c0a0608ae23
taufikprabowo1121 <solidiercyber@gmail.com>
2017-08-23 16:34:13 +0700

    NetModule1

---
+++ NetMod1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter;
+            const double pi = 3.14159;
+            counter = 1;
+
+            int[] radii = { 15, 32, 108, 74, 9 };
+            foreach(int i in radii)
+            {
+                //declaration statement with initializer
+                double circumference = pi * (2 * i);
+                Console.WriteLine("Radius of circel #{0} is {1} Circumference = {2:N2}", counter, i, circumference);
+                counter++;
+                Console.ReadKey();
+            }
+        }
+    }
+}
